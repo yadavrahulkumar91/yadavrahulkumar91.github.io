20eb499862a70afc3403ea8204e2afb23c7b3e51
Rahul Kumar Yadav <yadavrahulkumar91@gmail.com>
2023-05-19 02:28:08 +0545

    gallery

---
+++ script/gallery.js
@@ -0,0 +1,19 @@
+ // Open fullscreen mode
+ function openFullscreen(title, description) {
+    const fullscreen = document.querySelector('.fullscreen');
+    const image = document.querySelector('.fullscreen .image');
+    const titleElement = document.querySelector('.fullscreen-title');
+    const descriptionElement = document.querySelector('.fullscreen-description');
+
+    image.src = event.target.src;
+    titleElement.textContent = title;
+    descriptionElement.textContent = description;
+    fullscreen.style.display = 'flex';
+  }
+
+
+  // Close fullscreen mode
+  function closeFullscreen() {
+    const fullscreen = document.querySelector('.fullscreen');
+    fullscreen.style.display = 'none';
+  }
