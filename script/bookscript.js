aacc2d6317f1726e83466fce20853d603cbb5fdf
Rahul Kumar Yadav <yadavrahulkumar91@gmail.com>
2023-05-20 21:12:18 +0545

    update IOE physics

--- script/bookscript.js
+++ script/bookscript.js
@@ -47,7 +47,7 @@ document.head.appendChild(script3);
 
 
 
-
+//form
 function checkAnswer(button) {
   // Get the question div and the selected radio button
   var questionDiv = button.closest('.questions');
@@ -82,5 +82,27 @@ function checkAnswer(button) {
       answerDiv.textContent = 'Incorrect.';
       solutionDiv.style.display = 'block';
   }
+}
+
+
+//Gallery
+ // Open fullscreen mode
+ function openFullscreen(title, description) {
+  const fullscreen = document.querySelector('.fullscreen');
+  const image = document.querySelector('.fullscreen .image');
+  const titleElement = document.querySelector('.fullscreen-title');
+  const descriptionElement = document.querySelector('.fullscreen-description');
+
+  image.src = event.target.src;
+  titleElement.textContent = title;
+  descriptionElement.textContent = description;
+  fullscreen.style.display = 'flex';
+}
+
+
+// Close fullscreen mode
+function closeFullscreen() {
+  const fullscreen = document.querySelector('.fullscreen');
+  fullscreen.style.display = 'none';
 }
 
