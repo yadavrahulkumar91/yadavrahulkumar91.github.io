4f897a56a5b97ca5b3c84a5b082c8d3c51891f5a
Rahul Kumar Yadav <yadavrahulkumar91@gmail.com>
2023-06-11 03:02:48 +0545

    signin and sign up

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD4f2qv32PaDfYdo5m8qdN_pxGSEX6QUhw",
+    authDomain: "gamechanger-f5da7.firebaseapp.com",
+    databaseURL: "https://gamechanger-f5da7-default-rtdb.firebaseio.com",
+    projectId: "gamechanger-f5da7",
+    storageBucket: "gamechanger-f5da7.appspot.com",
+    messagingSenderId: "358268649157",
+    appId: "1:358268649157:web:8439649bc02f3a6cb8faac",
+    measurementId: "G-QBH1QYTY8V"
+  };
