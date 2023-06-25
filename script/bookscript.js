cc172a428700e543ca38c1c2f63adaa073c7f290
Rahul Kumar Yadav <yadavrahulkumar91@gmail.com>
2023-06-25 13:31:09 +0545

    Descripton for practicals

--- script/bookscript.js
+++ script/bookscript.js
@@ -322,3 +322,124 @@ function showDetails() {
 
  // Set up navigation buttons
 // addNavigationButtons();
+
+
+
+
+
+
+
+
+
+
+
+const menuToggleBtn = document.getElementById('menu-toggle-btn-id');
+  const sideBar = document.querySelector('.sidebar');
+  
+  menuToggleBtn.addEventListener('click', () => {
+    sideBar.classList.toggle('show-sidebar');
+    
+  });
+
+
+
+
+
+
+
+// Get the sidebar container
+var sidebar = document.getElementById('sidebar-id');
+
+// Get all heading elements
+var container = document.querySelector('.container');
+var headings = container.querySelectorAll('h1, h2, h3, h4, h5, h6');
+
+
+
+// Initialize variables to keep track of the parent div and current currentLevel
+var parentDiv = sidebar;
+var previousLevel = 0;
+
+// Iterate over each heading element
+for (var i = 0; i < headings.length; i++) {
+  var heading = headings[i];
+  var currentLevel = parseInt(heading.tagName.charAt(1));
+
+  // Generate a unique ID for the heading element
+  var headingId = 'sidebar-heading-' + i;
+
+  // Set the generated ID on the heading element
+  heading.setAttribute('id', headingId);
+
+
+  // Create a new div for the current currentLevel if necessary
+  if (currentLevel > previousLevel) {
+    /*
+
+    var sidebarCollapseButton = document.createElement('button');
+    sidebarCollapseButton.className='sidebar-collapse-button';
+
+    parentDiv.appendChild(sidebarCollapseButton);
+
+*/
+
+    var div = document.createElement('div');
+    div.className = 'h' + currentLevel;
+    parentDiv.appendChild(div);
+    
+    parentDiv = div;
+
+    
+
+
+  }
+
+  
+  // Move up the hierarchy if necessary
+  else if (currentLevel < previousLevel) {
+    while (previousLevel > currentLevel) {
+      parentDiv = parentDiv.parentNode;
+      previousLevel--;
+    }
+  }
+
+  // Create a link element
+  var link = document.createElement('a');
+  link.textContent = heading.textContent;
+  link.href = '#' + headingId;
+  parentDiv.appendChild(link);
+
+  
+  parentDiv.appendChild(document.createElement('br'));
+
+ 
+
+  // Update the current currentLevel
+  previousLevel = currentLevel;
+}
+
+
+
+
+
+
+
+/*
+
+
+
+
+var toggleButtons = document.querySelectorAll('.sidebar-collapse-button');
+
+toggleButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    var childNodes = Array.from(this.parentNode.childNodes); // Get an array of child nodes
+    childNodes.forEach(function(node) {
+      if (node.nodeType === 1 && node !== button) {
+        node.classList.toggle('open'); // Toggle the 'open' class for child nodes
+      }
+    });
+  });
+});
+
+*/
